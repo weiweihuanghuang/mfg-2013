281e809ef9573eafd927383c4cc4c7e30eade99a
Wei Huang <weiandhimself@gmail.com>
2013-07-02 13:50:08 +0200

    New parameters, glyphs.mf and b.mf
    To include 2 virtual masters

--- param_script.py
+++ param_script.py
@@ -12,57 +12,58 @@ import sys
 
 string = "% parameter file \n"
 
-string = string + "incx:=0.5;\n"
-
-# string = string + "incx:="+incx + ";\n"
+#global parameter
+string = string + "incx:=0;\n"
 string = string + "font_size:=12pt#;\n"  
 string = string + "ht#:=10pt#; \n"      
 string = string + "u#:=1pt#; \n"      			 
-string = string + "px#:=.8pt#; \n"  
-string = string + "penx:=1; \n"      					    					
-string = string + "superness:=1.08; \n"  
-string = string + "xheight:=1.0; \n"
-string = string + "mean#:=4.10pt# * xheight; \n"
-string = string + "des#:=2.45pt#; \n"
-string = string + "asc#:=0.72ht#; \n"
-string = string + "cap#:=0.65ht#; \n"
-string = string + "width:=1; \n"
-string = string + "widthcomp:=.1; \n"
-string = string + "capital:=1; \n"
-string = string + "ascender:=1; \n"
-string = string + "descender:=1; \n"
-string = string + "over#:=0.1u#; \n"
-string = string + "baseline:=0; \n"
-string = string + "winkel:=1; \n"
-string = string + "rotationA:=0; \n"
-string = string + "rotationB:=0; \n"
+string = string + "superness:=1.2; \n"  
+string = string + "max_stemcut:=40; \n"
 
+#local parameter A at incx = 0
+string = string + "A_px#:=0.1pt#; \n"      					
+string = string + "A_mean#:=6.12pt#; \n"
+string = string + "A_des#:=2.45pt#; \n"
+string = string + "A_asc#:=0.8ht#; \n"
+string = string + "A_cap#:=0.8ht#; \n"
+string = string + "A_width:=1; \n"
+string = string + "A_xheight:=1; \n"
+string = string + "A_capital:=1; \n"
+string = string + "A_ascender:=1; \n"
+string = string + "A_descender:=1; \n"
+string = string + "A_inktrap:=10; \n"
+string = string + "A_stemcut:=20; \n"
+string = string + "A_skeleton#:=0pt#; \n"
 
-# extra parameters
+#local parameter B at incx = 1
+string = string + "B_px#:=0.1pt#; \n"      					
+string = string + "B_mean#:=6.12pt#; \n"
+string = string + "B_des#:=2.45pt#; \n"
+string = string + "B_asc#:=0.8ht#; \n"
+string = string + "B_cap#:=0.8ht#; \n"
+string = string + "B_width:=1.05; \n"
+string = string + "B_xheight:=1; \n"
+string = string + "B_capital:=1; \n"
+string = string + "B_ascender:=1; \n"
+string = string + "B_descender:=1; \n"
+string = string + "B_inktrap:=10; \n"
+string = string + "B_stemcut:=0; \n"
+string = string + "B_skeleton#:=0.02pt#; \n"
 
-string = string + "width_a:=1.1; \n"
-string = string + "width_B:=1; \n"
-string = string + "width_N:=1; \n"
+#glyph parameter
 string = string + "width_n:=1; \n"
-string = string + "width_o:=1; \n"
-string = string + "width_g:=1; \n"
-string = string + "width_b:=1; \n"
-string = string + "width_c:=1; \n"
-string = string + "width_d:=1; \n"
-string = string + "width_e:=1; \n"
-string = string + "width_f:=1; \n"
-string = string + "width_g:=1; \n"
-string = string + "width_h:=1; \n"
-string = string + "width_i:=1; \n"
-string = string + "width_j:=1; \n"
-string = string + "width_k:=1; \n"
-
-
+string = string + "width_nL:=0; \n"
+string = string + "width_nR#:=0pt#; \n"
 
 
 string = string + "  \n"
 string = string + "input glyphs \n"
 string = string + "bye \n"
 string = string + " \n"
+
+#string = string + "contr:=0; \n"
+#progressive contrast increase
+#string = string + "contr:=-30incx+35pt#; \n"
+
 
 print string
